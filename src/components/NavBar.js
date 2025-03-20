788d3b5dd3d872a7f93543e53de5cfa0c9ef6442
Onizaro <nizaroumehdi@gmail.com>
2025-03-20 23:10:06 +0100

    navbar half done

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        <div class="navbar">
+            <ul class="nav-items">
+                <li><input placeholder="search"></input></li>
+                <li><input placeholder="search"></input></li>
+                <li><input placeholder="search"></input></li>
+            </ul>
+        </div>
+    );
+}
+
+export default NavBar;
